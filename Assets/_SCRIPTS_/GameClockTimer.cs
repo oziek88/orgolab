7ae25bd36e3859efff4528c7c4b3a792016b0ab5
Ian <oziek88@gmail.com>
2023-02-04 12:11:07 -0500

    text more readable and scare coutdown working

--- Assets/_SCRIPTS_/GameClockTimer.cs
+++ Assets/_SCRIPTS_/GameClockTimer.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameClockTimer:MonoBehaviour {
 
     public float startTime;
     public float Countdown;
     private float levelScore = 0;
-    public UnityEngine.UI.Text GameClock;
+    public Text gameClock;
 
     private bool minusPoints = false;
     private bool isFreeplay;
 
     public bool runTimer = false;
 
-    public GameObject Controller;
+    public Spawning spawningController;
+    public GameObject scoreBoard;
+    public ScoreKeeper scoreKeeper;
+    public Toggle pause;
+
+    private int smallFontSize = 180;
+    private int bigFontSize = 200;
+    
 
     // Use this for initialization
     void Start() {
@@ -22,86 +30,96 @@ public class GameClockTimer:MonoBehaviour {
         Countdown = startTime;
         isFreeplay = GameObject.Find("MainMenuCanvas") != null ? GameObject.Find("MainMenuCanvas").GetComponent<StartGame>().isFreeplay : false;
         if(isFreeplay) {
-            GameClock.enabled = false;
-            GameObject.Find("ScoreBoard").SetActive(false);
-        };
+            gameClock.enabled = false;
+            scoreBoard.SetActive(false);
+        } else {
+            gameClock.fontSize = smallFontSize;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate() {
 
-        if(Controller.GetComponent<Spawning>().completedReaction == true && Countdown > 0) {
+        if(spawningController.completedReaction == true && Countdown > 0) {
             runTimer = false;
             if(levelScore == 0) {
                 levelScore = Countdown;
                 Countdown = startTime;
-                GameObject.Find("TotalScore").GetComponent<ScoreKeeper>().totalScore += levelScore;
+                scoreKeeper.totalScore += levelScore;
             }
 
         } else if(runTimer && Countdown > 0) {
             if(levelScore != 0) {
                 levelScore = 0;
-                GameClock.color = new Color(0, 255, 223, 0.15f);
-                GameClock.fontSize = 72;
+                gameClock.color = new Color(0, 255, 223, 0.15f);
+                gameClock.fontSize = smallFontSize;
             }
 
             Countdown -= 1;
 
             if(!minusPoints) {
-                GameClock.text = Countdown.ToString();
+                gameClock.text = Countdown.ToString();
             }
 
             if(Countdown == 1000) {
-                GameClock.color = new Color(255, 206, 0, 0.4f);
-                GameClock.fontSize = 80;
+                gameClock.color = new Color(255, 206, 0, 0.4f);
+                gameClock.fontSize = bigFontSize;
                 if(isFreeplay) {
                     Countdown += 1;
                 }
             }
             if(Countdown == 995) {
-                GameClock.color = new Color(255, 206, 0, 0.2f);
-                GameClock.fontSize = 72;
+                gameClock.color = new Color(255, 206, 0, 0.2f);
+                gameClock.fontSize = smallFontSize;
             }
             if(Countdown == 500) {
-                GameClock.color = new Color(255, 0, 0, 0.4f);
-                GameClock.fontSize = 80;
+                gameClock.color = new Color(255, 0, 0, 0.4f);
+                gameClock.fontSize = bigFontSize;
             }
             if(Countdown == 495) {
-                GameClock.color = new Color(255, 0, 0, 0.2f);
-                GameClock.fontSize = 72;
+                gameClock.color = new Color(255, 0, 0, 0.2f);
+                gameClock.fontSize = smallFontSize;
             }
             if(Countdown == 100) {
-                GameClock.color = new Color(255, 0, 0, 0.4f);
-                GameClock.fontSize = 80;
+                gameClock.color = new Color(255, 0, 0, 0.4f);
+                gameClock.fontSize = bigFontSize;
             }
-        } else if(Controller.GetComponent<Spawning>().completedReaction == false && Countdown <= 0) {
-            GameClock.text = "You Lose";
-            GameClock.fontSize = 40;
-        } else if(Controller.GetComponent<Spawning>().completedReaction == true && Countdown <= 0) {
-            GameClock.text = "Never Give Up!";
-            GameClock.color = new Color(0, 206, 0, 0.4f);
-            GameClock.fontSize = 28;
+        } else if(spawningController.completedReaction == false && Countdown <= 0) {
+            gameClock.text = "You Lose";
+            gameClock.fontSize = 100;
+        } else if(spawningController.completedReaction == true && Countdown <= 0) {
+            gameClock.text = "Never Give Up!";
+            gameClock.color = new Color(0, 206, 0, 0.4f);
+            gameClock.fontSize = 80;
         }
     }
 
     public IEnumerator MinusPoints(float points) {
         minusPoints = true;
 
-        GameClock.color = new Color(255, 0, 0, 0.4f);
-        GameClock.fontSize = 80;
-        GameClock.text = "-" + points.ToString();
+        gameClock.color = new Color(255, 0, 0, 0.4f);
+        gameClock.fontSize = bigFontSize;
+        gameClock.text = "-" + points.ToString();
         Countdown -= points;
         yield return new WaitForSeconds(1);
         if(Countdown > 1001) {
-            GameClock.color = new Color(0, 255, 223, 0.2f);
+            gameClock.color = new Color(0, 255, 223, 0.2f);
         } else if(Countdown <= 1001 && Countdown > 501) {
-            GameClock.color = new Color(255, 206, 0, 0.2f);
+            gameClock.color = new Color(255, 206, 0, 0.2f);
         } else if(Countdown <= 501) {
-            GameClock.color = new Color(255, 0, 0, 0.2f);
+            gameClock.color = new Color(255, 0, 0, 0.2f);
         }
-        GameClock.fontSize = 72;
-        GameClock.text = Countdown.ToString();
+        gameClock.fontSize = smallFontSize;
+        gameClock.text = Countdown.ToString();
 
         minusPoints = false;
+    }
+
+    public void PauseGame() {
+        if(pause.isOn) {
+            Time.timeScale = 0;
+        } else {
+            Time.timeScale = 1;
+        }
     }
 }
